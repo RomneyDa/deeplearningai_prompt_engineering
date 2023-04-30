07bfefd7d86fcf9862022258f280372d2a1fdf79
Dallin Romney <dallinromney@gmail.com>
2023-04-29 19:43:33 -0600

    guidlines through summarizing

---
+++ prompts.py
@@ -0,0 +1,59 @@
+
+
+
+
+guidelines_tactic_1_insert_text = f"""
+You should express what you want a model to do by \ 
+providing instructions that are as clear and \ 
+specific as you can possibly make them. \ 
+This will guide the model towards the desired output, \ 
+and reduce the chances of receiving irrelevant \ 
+or incorrect responses. Don't confuse writing a \ 
+clear prompt with writing a short prompt. \ 
+In many cases, longer prompts provide more clarity \ 
+and context for the model, which can lead to \ 
+more detailed and relevant outputs.
+"""
+
+guidelines_tactic_1 = f"""
+Summarize the text delimited by triple backticks \ 
+into a single sentence.
+```{guidelines_tactic_1_insert_text}```
+"""
+
+# Ask for a structured output
+guidelines_tactic_2 = f"""
+Generate a list of three made-up book titles along \ 
+with their authors and genres. 
+Provide them in JSON format with the following keys: 
+book_id, title, author, genre.
+"""
+
+guidelines_tactic_3_inner_text = f"""
+Making a cup of tea is easy! First, you need to get some \ 
+water boiling. While that's happening, \ 
+grab a cup and put a tea bag in it. Once the water is \ 
+hot enough, just pour it over the tea bag. \ 
+Let it sit for a bit so the tea can steep. After a \ 
+few minutes, take out the tea bag. If you \ 
+like, you can add some sugar or milk to taste. \ 
+And that's it! You've got yourself a delicious \ 
+cup of tea to enjoy.
+"""
+guidelines_tactic_3 = f"""
+You will be provided with text delimited by triple quotes. 
+If it contains a sequence of instructions, \ 
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - …
+…
+Step N - …
+
+If the text does not contain a sequence of instructions, \ 
+then simply write \"No steps provided.\"
+
+\"\"\"{text_1}\"\"\"
+"""
+
+
