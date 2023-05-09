63cebe0407c15679aab9831239552dfea6d383f5
Dallin Romney <dallinromney@gmail.com>
2023-05-08 21:33:42 -0600

    docstring on get_completion

--- lib.py
+++ lib.py
@@ -7,20 +7,18 @@ _ = load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# def get_completion(prompt, model="gpt-3.5-turbo"):
-#     messages = [{"role": "user", "content": prompt}]
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=messages,
-#         temperature=0, # this is the degree of randomness of the model's output
-#     )
-#     return response.choices[0].message["content"]
 
-
-# With temperature
 def get_completion(
     prompt, model="gpt-3.5-turbo", temperature=0
 ):  # Andrew mentioned that the prompt/ completion paradigm is preferable for this class
+    """
+    This function takes a prompt and returns a completion from the OpenAI API
+    Reference: https://platform.openai.com/docs/api-reference
+
+    Optionally, you can specify the model and temperature
+    Info on models: https://platform.openai.com/docs/models
+    Temperature: decimal between 0 and 1; 0 is deterministic (always the same result), 1 is highly random
+    """
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
